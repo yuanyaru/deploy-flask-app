7af9bda6d5531832d8950cf083526b7bacaadccc
yuanyr <398916756@qq.com>
2019-09-19 03:21:16 +0100

    update url

--- deploy-blog.sh
+++ deploy-blog.sh
@@ -5,4 +5,5 @@
 # 运行容器
 # docker run --name flask-blog -p 5000:5000 -v /home/yyr/docker-flask-app:/code/blog --restart always --privileged=true -it flask-blog
 
+# pip install docker-compose
 docker-compose up
